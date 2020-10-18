a411b975387e3a6ddee786d62903c90ce8631f74
4a454646 <4a.45.46.46@gmail.com>
2020-10-18 11:55:01 -0700

    begin save system
    task names are now stored in memory

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SaveSystem : MonoBehaviour {
+    public string playerPrefString;
+    public void saveValueToPrefs() {
+        // very wonky save system. couldn't get it to properly work, so the solution here is to set this gameobject's name to be that of the input field (when finished editing), and read off this function. a new gameobject with this script is used for text field
+        PlayerPrefs.SetString(playerPrefString, name);
+        print($"saved {name} under {playerPrefString}");
+    }
+}
